d4c89cada368d30ab545ec479e399a8b3e914b20
Viachaslau Kalevich <cto@noirix.com>
2022-07-19 21:59:43 +0300

    Test new git update

--- src/main/java/com/noirix/TestNewGit.java
+++ src/main/java/com/noirix/TestNewGit.java
@@ -1,4 +1,8 @@
 package com.noirix;
 
 public class TestNewGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Wrold");
+        System.out.println(2+2);
+    }
 }
