1c15968beea617549b4b4944a9b58dbf4a116fa5
Slava Kalevich <pokerslava96@mail.ru>
2022-07-19 21:39:50 +0300

    Revert "test rebase"
    This reverts commit 06aeeedae17e5135ad40de7cf7c45cd33e8312f7.

--- src/main/java/com/noirix/TestGit.java
+++ src/main/java/com/noirix/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
 
-        System.out.println("Hello Rebase!");
+        System.out.println("Hello Git!");
         System.out.println("Hel GitGit!");
     }
 }
--- src/main/java/com/noirix/TestNewGit.java
+++ src/main/java/com/noirix/TestNewGit.java
@@ -1,7 +1,4 @@
 package com.noirix;
 
 public class TestNewGit {
-    public static void main(String[] args) {
-        System.out.println("Hello rebase");
-    }
 }
