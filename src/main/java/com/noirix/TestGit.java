691b3f04d95f886526c95fb8ac3ef317f78d159a
Viachaslau Kalevich <cto@noirix.com>
2022-07-19 21:29:58 +0300

    test rebase 2

--- src/main/java/com/noirix/TestGit.java
+++ src/main/java/com/noirix/TestGit.java
@@ -2,7 +2,7 @@ package com.noirix;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello Git!");
 
         System.out.println("Hello Git!");
         System.out.println("Hel Force!");
