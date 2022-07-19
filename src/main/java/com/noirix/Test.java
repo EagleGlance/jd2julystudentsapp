3312f8a50cb9483bc28433f1a33c077e8598334d
Viachaslau Kalevich <cto@noirix.com>
2022-07-19 20:19:35 +0300

    Initial commit

---
+++ src/main/java/com/noirix/Test.java
@@ -0,0 +1,12 @@
+package com.noirix;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world with help of Maven!");
+
+        String s = RandomStringUtils.randomAlphabetic(10);
+        System.out.println(s);
+    }
+}
