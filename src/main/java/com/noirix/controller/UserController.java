83727685ec9a30492bfb45ebe7c342c17e5f3725
Viachaslau Kalevich <cto@noirix.com>
2022-08-30 20:35:24 +0300

    Spring MVC: one object view

--- src/main/java/com/noirix/controller/UserController.java
+++ src/main/java/com/noirix/controller/UserController.java
@@ -3,9 +3,9 @@ package com.noirix.controller;
 import com.noirix.domain.User;
 import com.noirix.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +18,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/users")
+    @GetMapping("/users")
     public ModelAndView findAllUsers() {
         List<User> users = userService.findAll();
 
@@ -26,8 +26,25 @@ public class UserController {
         model.addObject("user", "Slava");
         model.addObject("users", users);
 
-        model.setViewName("hello");
+        model.setViewName("users");
 
         return model;
     }
+
+    @GetMapping("/users/{id}")
+    public ModelAndView findUserById(@PathVariable String id) {
+
+        //We have added id parsing and number format checking
+        long userId = Long.parseLong(id);
+        User user = userService.findById(userId);
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("userName", user.getUserName());
+        model.addObject("user", user);
+
+        model.setViewName("user");
+
+        return model;
+    }
+
 }
