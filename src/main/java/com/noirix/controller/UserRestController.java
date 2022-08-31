88f83bec495bb7e71aa3290a1d42a1bac4bda6f9
Viachaslau Kalevich <cto@noirix.com>
2022-08-31 20:51:12 +0300

    Spring REST: POST Request User Creation

--- src/main/java/com/noirix/controller/UserRestController.java
+++ src/main/java/com/noirix/controller/UserRestController.java
@@ -1,5 +1,6 @@
 package com.noirix.controller;
 
+import com.noirix.controller.requests.UserCreateRequest;
 import com.noirix.controller.requests.UserSearchRequest;
 import com.noirix.domain.User;
 import com.noirix.service.UserService;
@@ -8,11 +9,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.Timestamp;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -47,4 +53,38 @@ public class UserRestController {
 
         return new ResponseEntity<>(model, HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> findUserById(@PathVariable String id) {
+
+        //We have added id parsing and number format checking
+        long userId = Long.parseLong(id);
+
+        return new ResponseEntity<>(Collections.singletonMap("user", userService.findById(userId)), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@RequestBody UserCreateRequest createRequest) {
+
+        User user = new User();
+        user.setUserName(createRequest.getUserName());
+        user.setSurname(createRequest.getSurname());
+        user.setBirth(new Timestamp(new Date().getTime()));
+        user.setCreationDate(new Timestamp(new Date().getTime()));
+        user.setModificationDate(new Timestamp(new Date().getTime()));
+        user.setIsDeleted(false);
+        user.setWeight(createRequest.getWeight());
+
+        userService.create(user);
+
+        List<User> users = userService.findAll();
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("user", user.getUserName());
+        model.put("users", users);
+
+        return new ResponseEntity<>(model, HttpStatus.CREATED);
+    }
+
+
 }
