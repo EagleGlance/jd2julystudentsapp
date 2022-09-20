7e6df6008547ddcad24e09a34408aba0a17086a8
Viachaslau Kalevich <cto@noirix.com>
2022-09-20 20:05:21 +0300

    Hibernate Many to Many mapping

---
+++ src/main/java/com/noirix/domain/hibernate/HibernateRole.java
@@ -0,0 +1,47 @@
+package com.noirix.domain.hibernate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.noirix.domain.SystemRoles;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class HibernateRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    @Enumerated(EnumType.STRING)
+    private SystemRoles roleName;
+
+    @Column(name = "creation_date")
+    private Timestamp creationDate;
+
+    @Column(name = "modification_date")
+    private Timestamp modificationDate;
+
+    @ManyToMany
+    @JoinTable(name = "l_role_user",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    @JsonIgnoreProperties("roles")
+    private List<HibernateUser> users;
+}
--- src/main/java/com/noirix/domain/hibernate/HibernateUser.java
+++ src/main/java/com/noirix/domain/hibernate/HibernateUser.java
@@ -1,15 +1,23 @@
 package com.noirix.domain.hibernate;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.noirix.domain.Gender;
 import lombok.Data;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 @Entity
@@ -17,6 +25,7 @@ import java.sql.Timestamp;
 public class HibernateUser {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "user_name")
@@ -29,12 +38,15 @@ public class HibernateUser {
     private Timestamp birth;
 
     @Column(name = "is_deleted")
+    @JsonIgnore
     private Boolean isDeleted;
 
     @Column(name = "creation_date")
+    @JsonIgnore
     private Timestamp creationDate;
 
     @Column(name = "modification_date")
+    @JsonIgnore
     private Timestamp modificationDate;
 
     @Column
@@ -44,9 +56,14 @@ public class HibernateUser {
     private String userLogin;
 
     @Column(name = "user_password")
+    @JsonIgnore
     private String userPassword;
 
     @Column(name = "gender")
     @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    @ManyToMany(mappedBy = "users", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnoreProperties("users")
+    private List<HibernateRole> roles;
 }
