feb892f8db09ddea71baa92de30348158f6565ee
Viachaslau Kalevich <cto@noirix.com>
2022-10-13 21:38:40 +0300

    Spring Converters: with linked entities fix

--- api/src/main/java/com/noirix/controller/springdata/UserController.java
+++ api/src/main/java/com/noirix/controller/springdata/UserController.java
@@ -2,14 +2,17 @@ package com.noirix.controller.springdata;
 
 import com.noirix.controller.requests.UserCreateRequest;
 import com.noirix.domain.Gender;
+import com.noirix.domain.hibernate.HibernateRole;
 import com.noirix.domain.hibernate.HibernateUser;
 import com.noirix.repository.jdbctemplate.RoleRepositoryInterface;
+import com.noirix.repository.springdata.RolesSpringDataRepository;
 import com.noirix.repository.springdata.UserSpringDataRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +32,8 @@ import java.util.Map;
 public class UserController {
 
     private final UserSpringDataRepository repository;
+
+    private final RolesSpringDataRepository rolesSpringDataRepository;
 
     private final RoleRepositoryInterface roleRepository;
 
@@ -53,13 +60,29 @@ public class UserController {
     public ResponseEntity<Object> createUser(@RequestBody UserCreateRequest createRequest) {
 
         HibernateUser user = converter.convert(createRequest, HibernateUser.class);
-        HibernateUser createdUser = repository.save(user);
+        HibernateUser createdUser = repository.save(setRoles(user));
 
-        repository.createRoleRow(createdUser.getId(), roleRepository.findById(1L).getId());
+        //repository.createRoleRow(createdUser.getId(), roleRepository.findById(1L).getId());
 
         Map<String, Object> model = new HashMap<>();
         model.put("user", repository.findById(createdUser.getId()).get());
 
         return new ResponseEntity<>(model, HttpStatus.CREATED);
+    }
+
+    private HibernateUser setRoles(HibernateUser user) {
+        Set<HibernateRole> roles = user.getRoles();
+
+        Set<HibernateRole> updatedRoles = new HashSet<>();
+
+        if (!CollectionUtils.isEmpty(roles)) {
+            updatedRoles.addAll(roles);
+        }
+        updatedRoles.add(rolesSpringDataRepository.findById(1L).get());
+        updatedRoles.add(rolesSpringDataRepository.findById(2L).get());
+
+        user.setRoles(updatedRoles);
+
+        return user;
     }
 }
