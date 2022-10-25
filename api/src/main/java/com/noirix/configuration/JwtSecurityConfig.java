72cf1c573b021ef63f360059ea12f6702d2a2a83
Viachaslau Kalevich <cto@noirix.com>
2022-10-25 18:58:42 +0300

    Spring Profiles

--- api/src/main/java/com/noirix/configuration/JwtSecurityConfig.java
+++ api/src/main/java/com/noirix/configuration/JwtSecurityConfig.java
@@ -3,9 +3,11 @@ package com.noirix.configuration;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @ConfigurationProperties("jwt")
+//@Profile("dev")
 @Data
 public class JwtSecurityConfig {
 
